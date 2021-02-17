4c196d6e5f46f2afa2c52100b9b21bc12300a8b5
utkarsh8021 <mishra8021@gmail.com>
2021-02-17 22:46:51 +0530

    main

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+dbPassword = 'mongodb+srv://utkarsh:utk98@cluster0.ao51t.mongodb.net/users?retryWrites=true&w=majority';
+
+module.exports = {
+    mongoURI: dbPassword
+};
---
+++ routes/index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const User = require('../models/User');
+var ObjectID = require('mongodb').ObjectID;
+const router = express.Router();
+const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
+
+// Welcome Page
+router.get('/', forwardAuthenticated, (req, res) => res.render('welcome'));
+
+//About Page
+router.get('/about', forwardAuthenticated, (req,res) => res.render('about'));
+
+//About Page
+router.get('/services', forwardAuthenticated, (req,res) => res.render('services'));
+
+//About Page
+router.get('/project', forwardAuthenticated, (req,res) => res.render('project'));
+//About Page
+router.get('/contact', forwardAuthenticated, (req,res) => res.render('contact'));
+
+// Dashboard
+router.get('/dashboard', ensureAuthenticated, function (req, res) {
+  User.find({}, function(err, produtos) {
+      if (err){
+          console.log(err);
+      }else{
+          res.render('business_contacts.ejs', { data: produtos});
+          }
+      });
+});
+
+router.post('/bussiness_contacts/edit/:id', function(req, res){
+  let data = req.body;
+  let id = new ObjectID(req.params.id);
+  data._id = id;
+  User.findByIdAndUpdate(id, data).then(function(err) {
+    console.log(err, 'kkkkk');
+    User.findOne().then(function(items) {
+      console.log(items)
+      customers = items;
+      res.redirect('/dashboard');
+    });
+  });
+});
+
+
+
+
+module.exports = router;
